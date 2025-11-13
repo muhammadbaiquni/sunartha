1308cba33daf093aadff9c9febfb0ddf18322b9c
Muhammad Baiquni <muhammad.baiquni@gmail.com>
2025-11-14 00:52:17 +0700

    hilangkan data tidak perlu

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-
-namespace Sunartha;
+﻿namespace Sunartha;
 
 public class Program
 {
