8a7ad01f78bb44bfa6f455dff0b3c6239e374f4e
Muhammad Baiquni <muhammad.baiquni@gmail.com>
2025-11-14 00:51:37 +0700

    hilangkan data tidak perlu

--- Mobil.cs
+++ Mobil.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Sunartha;
 
-namespace Sunartha
+public class Mobil
 {
-    public class Mobil
+    public int IDRegistrasi { get; }
+    public string Tipe { get; }
+    public string Merk { get; }
+    public string Varian { get; }
+
+    public Mobil(int idRegistrasi, string tipe, string merk, string varian)
     {
-        public int IDRegistrasi { get; }
-        public string Tipe { get; }
-        public string Merk { get; }
-        public string Varian { get; }
+        IDRegistrasi = idRegistrasi;
+        Tipe = tipe;
+        Merk = merk;
+        Varian = varian;
+    }
 
-        public Mobil(int idRegistrasi, string tipe, string merk, string varian)
-        {
-            IDRegistrasi = idRegistrasi;
-            Tipe = tipe;
-            Merk = merk;
-            Varian = varian;
-        }
-
-        public override string ToString()
-        {
-            return $"{IDRegistrasi}, {Merk}, {Varian}";
-        }
+    public override string ToString()
+    {
+        return $"{IDRegistrasi}, {Merk}, {Varian}";
     }
 }
